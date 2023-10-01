bda6fb165acd53679aecc2ea5be0e9c2f10064d2
DianaManurung <dianamanurung12345@gmail.com>
2023-10-01 14:13:25 +0700

    P02

--- P02_01.java
+++ P02_01.java
@@ -1,1 +1,45 @@
-// NIM - Your Name
+// 12S23040 - Diana Hevila Manurung
+
+import java.util.*;
+import java.lang.Math;
+
+class P02 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String studiKasus;
+        String nim;
+        String tugas;
+        String dosen;
+        String haridanPengumpulanTugas;
+        String hasil1;
+        double tingkatKesulitan;
+        double hariDeadline;
+        String status;
+        double prioritas;
+
+        studiKasus = input.nextLine();
+        nim = input.nextLine();
+        tugas = input.nextLine();
+        dosen = input.nextLine();
+        haridanPengumpulanTugas = input.nextLine();
+        tingkatKesulitan = input.nextDouble();
+        hariDeadline = input.nextDouble();
+        status = input.nextLine();
+        prioritas = tingkatKesulitan * (1.0 / hariDeadline);
+        System.out.println("Prioritas :" + prioritas);
+        if (prioritas > 3) {
+            hasil1 = "Penting! Anda harus mengerjakan tugas segera";
+        } else {
+            if (prioritas >= 1.5 && prioritas <= 3) {
+                hasil1 = "Tugas ini memiliki prioritas menengah";
+            } else {
+                if (prioritas < 1.5) {
+                    hasil1 = "Tugas ini relatif ringan, namun jangan tunda terlalu lama";
+                }
+            }
+        }
+        System.out.println(studiKasus + "|" + nim + "|" + tugas + "|" + dosen + "|" + haridanPengumpulanTugas + "|" + tingkatKesulitan + "|" + hariDeadline + "|" + prioritas + "|" + status);
+    }
+}
+
--- P02_02.java
+++ P02_02.java
@@ -1,1 +1,42 @@
-//NIM -Nama
+//12S23040 -Diana Hevila Manurung
+
+import java.util.*;
+import java.lang.Math;
+
+class P02 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String namaGudang;
+        int kapasitasGudang;
+        int jumlahBukuSaatIni;
+        String aC;
+        String lantai;
+        String ketersediaanTeknologi;
+        int gudangElite;
+        int gudangStandar;
+        int gudangPerluPeningkatan;
+        int skor;
+        String hasil;
+
+        namaGudang = input.nextLine();
+        kapasitasGudang = input.nextInt();
+        jumlahBukuSaatIni = input.nextInt();
+        aC = input.nextLine();
+        lantai = input.nextLine();
+        ketersediaanTeknologi = input.nextLine();
+        skor = (int) ((double) jumlahBukuSaatIni / kapasitasGudang * 100);
+        if (skor >= 40) {
+            hasil = "GudangElite";
+        } else {
+            if (skor >= 25) {
+                hasil = "GudangStandar";
+            } else {
+                if (skor < 25) {
+                    hasil = "GudangPerluPeningkatan";
+                }
+            }
+        }
+        System.out.println(namaGudang + "|" + kapasitasGudang + "|" + jumlahBukuSaatIni + "|" + aC + "|" + lantai + "|" + ketersediaanTeknologi + "|" + skor + "|" + hasil);
+    }
+}
